6ba1d0d9be5c5bd02f64d4a5051e77b4ab1bf9c0
Eduardo Aguilera <laloao.2302@gmail.com>
2014-12-08 19:28:03 -0600

    Se agrego el metodo de buscar libro

--- App_Code/MiWebService.cs
+++ App_Code/MiWebService.cs
@@ -185,6 +185,69 @@ public class MiWebService : System.Web.Services.WebService
         return null;
     }
 
+    [WebMethod]
+
+    public ContenidoLibro Buscarlibro(string busqueda)
+    {
+        MySqlConnection miConexionSQL = new MySqlConnection(Conexion.ObtenerCadenaConexion());
+        if (miConexionSQL.State == System.Data.ConnectionState.Closed)
+            miConexionSQL.Open();
+        if (miConexionSQL.State == System.Data.ConnectionState.Open)
+        {
+            MySqlCommand query = new MySqlCommand
+                ("SELECT * from (select libros.idLibro, libros.nombreLibro, libros.portada, libros.autorEnsayo, libros.genero, autores.nombreAutor, autores.apellidoAutor from libros, autores, autorlibro where libros.nombreLibro LIKE '%" + busqueda + "%' and autorlibro.idLibro = libros.idLibro and autorlibro.idAutor = autores.idAutor UNION select libros.idLibro, libros.nombreLibro, libros.portada, libros.autorEnsayo,  libros.genero, autores.nombreAutor, autores.apellidoAutor from libros, autores, autorlibro where autores.nombreAutor  LIKE '%" + busqueda + "%' and autorlibro.idLibro = libros.idLibro and autorlibro.idAutor = autores.idAutor  UNION select libros.idLibro, libros.nombreLibro, libros.portada, libros.autorEnsayo, libros.genero, autores.nombreAutor,  autores.apellidoAutor from libros, autores, autorlibro where autores.apellidoAutor LIKE '%" + busqueda + "%' and autorlibro.idLibro = libros.idLibro  and autorlibro.idAutor = autores.idAutor UNION select libros.idLibro, libros.nombreLibro, libros.portada, libros.autorEnsayo, libros.genero, autores.nombreAutor,  autores.apellidoAutor from libros, autores, autorlibro where libros.autorEnsayo LIKE '%" + busqueda +"%' and autorlibro.idLibro = libros.idLibro  and autorlibro.idAutor = autores.idAutor) a order by idLibro", miConexionSQL);
+            query.CommandType = System.Data.CommandType.Text;
+            query.CommandTimeout = 120;
+            MySqlDataReader reader = null;
+            DataTable tabla = new DataTable();
+
+            /*DataTable toma la primera columna de la tabla como primary key por defecto, asi que no van a salir libros
+            repetidos porque el idLibro no se puede repetir. El libro que tiene dos autores necesitamos que salga dos veces
+             para capturar ambos autores, asi que se agrega una nueva columna autoincrementable y a esa columna se le asigna
+             el primary key*/
+
+            tabla.Columns.Add(new System.Data.DataColumn("PK", typeof(System.Int32))); //agregar una nueva columna llamada PK
+            tabla.PrimaryKey = new DataColumn[1] { tabla.Columns["PK"] }; //asignar nueva columna como primary key
+            tabla.Columns["PK"].AutoIncrement = true; //autoincrement en nueva columna
+
+
+            query.Prepare();
+            reader = query.ExecuteReader();
+            tabla.Load(reader);
+            reader.Close();
+            query.Dispose();
+            miConexionSQL.Close();
+
+            if (tabla.Rows.Count > 0)
+            {
+                ContenidoLibro respuesta = new ContenidoLibro();
+                respuesta.IDLibro = tabla.Rows[0]["idLibro"].ToString();
+                respuesta.Titulo = tabla.Rows[0]["nombreLibro"].ToString();
+                respuesta.Autor = tabla.Rows[0]["nombreAutor"].ToString() + " " + tabla.Rows[0]["apellidoAutor"].ToString();
+                respuesta.Genero = tabla.Rows[0]["genero"].ToString();
+                respuesta.Editorial = tabla.Rows[0]["editorial"].ToString();
+                respuesta.Publicacion = tabla.Rows[0]["añoPublicacion"].ToString();
+                respuesta.Sinopsis = tabla.Rows[0]["sinopsis"].ToString();
+                respuesta.AutorEnsayo = tabla.Rows[0]["autorEnsayo"].ToString();
+                respuesta.Ensayo = tabla.Rows[0]["ensayo"].ToString();
+                respuesta.Portada = tabla.Rows[0]["portada"].ToString();
+
+                //for para agregar mas autores a un libro en el caso de que este tenga mas de un autor.
+                if (tabla.Rows.Count > 1)
+                {
+                    for (int i = 1; i < tabla.Rows.Count; i++)
+                    {
+                        respuesta.Autor += ", " + tabla.Rows[i]["nombreAutor"].ToString() + " " + tabla.Rows[i]["apellidoAutor"].ToString();
+                    }
+                }
+
+                return respuesta;
+            }
+        }
+
+        return null;
+    }
+
 
 
 }
